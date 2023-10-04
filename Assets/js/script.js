9848f7d380a9af5ceca72bceab6a8e14c21fe39f
Zephausxv <99859751+Zephausxv@users.noreply.github.com>
2023-10-04 14:17:36 -0400

    Added functional API calls

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,26 +1,74 @@
 var searchButton = $('#search-btn');
 var searchInput = $('#searchInput');
+var recentHistory = $('#recentHistory');
+var cityName = 'undefined';
+var lat = 0;
+var lon = 0;
+var temp = [];
+var wind = [];
+var humidity = [];
 
 function searchButtonHandler() {
-    var inputString = searchInput.val().toLowerCase();
-    
-    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + inputString + '&limit=1&appid=e9b8186425312aeb2aeb30949d61fa5c';
-
-    fetch(requestUrl).then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-      console.log(data[i].title);
+    if (searchInput.val() === '') {
+        console.log('Empty!');
+    }
+    else {
+        var cityHistoryString = localStorage.getItem('cityHistory');
+        var cityHistory = [];
+        if (cityHistoryString) {
+            cityHistory = JSON.parse(cityHistoryString);
         }
-    });
-    
-    
-    
-
+        cityHistory.push(searchInput.val().trim());
+        localStorage.setItem('cityHistory', JSON.stringify(cityHistory));
+        convertGeo();
+    }
 }
 
+function convertGeo() {
+    var inputString = searchInput.val().toLowerCase();
+    var requestURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + inputString + '&limit=1&appid=e9b8186425312aeb2aeb30949d61fa5c';
+
+    fetch(requestURL)
+    .then(response => {          
+        return response.json();
+    })
+    .then(data => {
+        cityName = data[0].name;
+        lat = data[0].lat;
+        lon = data[0].lon;
+        getAPI();
+    })
+    .catch(error => {
+    });
+}
+
+function getAPI() {
+    var requestForecast = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=e9b8186425312aeb2aeb30949d61fa5c&units=imperial'
+    fetch(requestForecast)
+    .then(response => {          
+        return response.json();
+    })
+    .then(data => {
+        for (var i = 0; i < data.list.length; i += 8) {
+            temp.push(data.list[i].main.temp);
+            wind.push(data.list[i].wind.speed);
+            humidity.push(data.list[i].main.humidity);
+        }
+        renderAPI();
+    })
+    .catch(error => {
+    });
+}
+
+function renderAPI() {
+    var cityHistory = JSON.parse(localStorage.getItem('cityHistory')) || [];
+
+    for (var i = 0; i < cityHistory.length; i++) {
+        var searchResults = $('<h4>');
+        searchResults.text(cityHistory[i]);
+        recentHistory.append(searchResults);
+    }
+}
 
 
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=e9b8186425312aeb2aeb30949d61fa5c
